c31eaadcf24c1f5edd69fe924bfb72a7ec0c153b
Victor Suarez <vasuarezag@cesde.net>
2022-08-24 20:56:32 -0500

    Se crea controlador Cinema

---
+++ src/controllers/controladorCinema.js
@@ -0,0 +1,92 @@
+let animes = Array({
+    nombre: 'Detective Conan',
+    genero: 'Misterio',
+    duracion: 940,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/DetectiveConan.jpg?alt=media&token=5f2f22ca-3225-45b4-99da-a2691fc406a5',
+    sinopsis: 'Trata sobre un joven de escuela preparatoria llamado Shinichi Kudo, que un día mientras estaba en un parque de atracciones siguió a unos hombres vestidos de negro que estaban haciendo tratos ilegales. De pronto uno de ellos coge a Shinichi por la espalda, lo golpea con un bate y lo droga con una pastilla que en teoría lo llevaría a su muerte. Pero esa pastilla tenía unos efectos secundarios: en vez de morir, encoge. Allí es cuando Shinichi Kudo de 17 años pasa a ser Conan Edogawa, de 7 años, un niño con mente de un adulto que busca la única y sola verdad.',
+    clasificacion: '+12',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'One Piece',
+    genero: 'Aventura',
+    duracion: 1040,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/OnePiece.jpg?alt=media&token=882ece4c-85f1-42f8-9452-727d6fe51b63',
+    sinopsis: 'Una historia épica de piratas, donde narra la historia de "Monkey D. Luffy" quien cuado tenia 7 años, comió accidentalmente una "Akuma no mi"(Futa del diablo) la cual le dio poderes de goma. Por otra parte "Gol D. Roger" conocido como "El rey de los Piratas" quien fuera ejecutado por la Marine, habló antes de morir, acerca de su famoso tesoro "One Piece" escondido en la "Gran line". Esta noticia desato la gran era de la piratas lanzando a incontables piratas a ese lugar, en busca de "One Piece" el tesoro perdido. Diez años después, Luffy inspirado en "Gol D. Roger" y un pirata de nombre Akagami no Shanks (Shanks el pelirrojo) se convierte en pirata deseando ser el próximo "Rey de los Piratas" y zarpar para conocer amigos y tener aventuras con ellos, teniendo como meta encontrar el "One Piece".',
+    clasificacion: '+12',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'Saint Seiya',
+    genero: 'Guerra',
+    duracion: 114,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/SaintSeiya.jpg?alt=media&token=ecef9cad-4092-465b-9274-b3b3ad83b69e',
+    sinopsis: 'Cuenta la historia de un grupo de jóvenes guerreros denominados Santos (o caballeros en varias traducciones), quienes luchan por proteger a la diosa griega Atenea de las fuerzas del mal. Para luchar, los Santos sólo utilizan su energía interior (denominada cosmos), sus puños y armaduras que les sirven de protección y que además están inspiradas en constelaciones o seres de la mitología griega y escandinava.',
+    clasificacion: '+16',
+    idioma: 'Sp'
+}, {
+    nombre: 'Samurai X',
+    genero: 'Guerra',
+    duracion: 95,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/SamuraiX.jpg?alt=media&token=b4b38c84-6a40-42fa-9634-53118c86a5a7',
+    sinopsis: 'La historia trata de un joven, el mayor asesino de la era Tokugawa hasta la Restauración Meiji, apodado Battousai. Este joven llamado Kenshin Himura, decide no volver a matar gente, y para poder pagar de alguna forma los asesinatos que cometió, decide proteger a las personas a su alrededor con la técnica más aterradora y poderosa en existencia: el Hiten Mitsurugi Ry?, aprendida gracias al adiestramiento de su maestro Seijuro Hiko , que lo acogió cuando era pequeño tras sobrevivir en el asesinato de la caravana de esclavos en que se encontraba, donde 3 hermanas lo habian adoptado como hermano menor. De esta manera conoce a Kaoru Kamiya, del dojo Kamiya Kasshin Ry?, Sanosuke, Yahiko y a otros personajes que pasan a ser parte de su vida, y motivo de existencia.',
+    clasificacion: '+18',
+    idioma: 'Sp'
+}, {
+    nombre: 'Tokio Ghoul',
+    genero: 'Ficcion',
+    duracion: 95,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/TokyoGhoul.jpg?alt=media&token=793bdb75-5d3c-43fa-8e75-3f8e1918879e',
+    sinopsis: 'Extraños asesinatos se están sucediendo uno tras otro en Tokyo. Debido a las pruebas encontradas en las escenas, la policía concluye que los ataques son obra de ghouls que se comen a las personas. Kaneki y Hide, dos compañeros de clase, llegan a la conclusión de que si nadie ha visto nunca a esos necrófagos es porque toman la apariencia de seres humanos para ocultarse. Poco sabían entonces de que su teoría sería más cierta de lo que pensaban cuando Kaneki es herido de gravedad por un monstruo y comienza a atraerle cada vez más la carne humana…',
+    clasificacion: '+16',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'Attack On Titan',
+    genero: 'Ficcion',
+    duracion: 25,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/aot.jpg?alt=media&token=a95956f8-fcfd-43c9-ae64-70b60a327b26',
+    sinopsis: 'La historia nos traslada a un mundo en el que la humanidad estuvo a punto de ser exterminada cientos de años atrás por los gigantes. Los gigantes son enormes, parecen no ser inteligentes y devoran seres humanos. Lo peor es que parece que lo hacen por placer y no por alimentarse. Una pequeña parte de la humanidad ha conseguido sobrevivir protegiéndose en una ciudad con unos altísimos muros, más altos que el mayor de los gigantes. La ciudad vive su vida tranquila, y hace más de 100 años que ningún gigante aparece por allí. Eren y su hermana adoptiva Mikasa son todavía unos adolescentes cuando ven algo horroroso: un gigante mucho mayor que todos los que la humanidad había conocido hasta el momento está destruyendo los muros de la ciudad. No pasa mucho tiempo hasta que los gigantes entran por el hueco abierto en el muro y comienzan a devorar a la gente',
+    clasificacion: '+16',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'Bleach',
+    genero: 'Aventura',
+    duracion: 366,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/bleach.jpg?alt=media&token=5e5ff316-925f-4256-872b-eb4ee20e85fd',
+    sinopsis: 'Kurosaki Ichigo es un estudiante de instituto de 15 años, que tiene una peculiaridad: es capaz de ver, oír y hablar con fantasmas. Pero no sabe hasta dónde puede abarcar la clasificación de espíritus, ni lo que conlleva el saberlo. Un buen día, una extraña chica de pequeña estatura que viste ropas negras de samurai entra en su cuarto. Se llama Rukia Kuchiki, y es una Shinigami (Dios de la Muerte). Ante la incredulidad de Ichigo, le explica que su trabajo es mandar a las almas buenas o plus a un lugar llamado la Sociedad de Almas, y eliminar a las almas malignas o hollows. Luego junto a Inoue Orihime, Ishida Ury y Sado Yasutora se veran envueltos en diferentes batallas, las cuales iran desarrollando sus diferentes habilidades que le otorgaran a cada uno su importancia en la serie.',
+    clasificacion: '+16',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'High School Of The Dead',
+    genero: 'Zombies',
+    duracion: 12,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/hsotd.jpg?alt=media&token=d1c30dc2-a30d-4e18-b28e-b00699954bdc',
+    sinopsis: 'Kurosaki Ichigo es un estudiante de instituto de 15 años, que tiene una peculiaridad: es capaz de ver, oír y hablar con fantasmas. Pero no sabe hasta dónde puede abarcar la clasificación de espíritus, ni lo que conlleva el saberlo. Un buen día, una extraña chica de pequeña estatura que viste ropas negras de samurai entra en su cuarto. Se llama Rukia Kuchiki, y es una Shinigami (Dios de la Muerte). Ante la incredulidad de Ichigo, le explica que su trabajo es mandar a las almas buenas o plus a un lugar llamado la Sociedad de Almas, y eliminar a las almas malignas o hollows. Luego junto a Inoue Orihime, Ishida Ury y Sado Yasutora se veran envueltos en diferentes batallas, las cuales iran desarrollando sus diferentes habilidades que le otorgaran a cada uno su importancia en la serie.',
+    clasificacion: '+18',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'Prince Of Tennis',
+    genero: 'Deportes',
+    duracion: 178,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/pot.jpg?alt=media&token=ded3e801-f5b9-4584-9ddc-b39d7ca03f0c',
+    sinopsis: 'Ryoma Echizen es un chico de 12 años cuyo propósito es superar a su padre quien fue un profesional en el tenis ya retirado. Él vivía en Estados Unidos con su familia. Pero ellos deciden mudarse a japón la cual es la tierra natal de su padre. Allí Ryoma entra al instituto Seishun Gakuen o también llamada Seigaku, entonces decide inscribirse en el club de tenis y allí comienzan todas sus aventuras donde Ryoma demostrara sus habilidades en el tenis y hará saber su nombre " el príncipe del tenis".',
+    clasificacion: '+12',
+    idioma: 'Jp Subtitulado'
+}, {
+    nombre: 'Vampire Knight',
+    genero: 'Deportes',
+    duracion: 13,
+    poster: 'https://firebasestorage.googleapis.com/v0/b/cinevs-28f3f.appspot.com/o/vkn.jpg?alt=media&token=627ecaff-c6a5-4b75-83a8-74f7253504ea',
+    sinopsis: 'Lo único que Yūki recuerda de su infancia es que, perdida en el invierno, fue atacada por un vampiro fuera de control, situación de la que la rescató otro vampiro, Kuran Kaname. Él la llevó con Kaien Cross, quien se convierte en su padre adoptivo y más adelante, en el director de la ""Academia Privada Cross"". Sin embargo, la academia de elite oculta un secreto: los alumnos de la Clase de Noche son en realidad vampiros. El director y Kuran Kaname creen que es posible una convivencia pacífica de humanos y vampiros. Yūki y Zero, un chico procedente de una familia de cazadores de vampiros cuyos padres fueron asesinados por uno de ellos cuatro años atrás, son los únicos de la Clase de Día que conocen el secreto y se ocupan de mantener el orden en la Academia. Zero odia a los vampiros más que a nada en el mundo, pero a su vez también oculta un gran secreto que lo tortura día a día',
+    clasificacion: '+12',
+    idioma: 'Jp Subtitulado'
+})
+
+// Recorriendo un arreglo en JS
+animes.forEach(function(anime) {
+    console.log(anime.nombre);
+    console.log(anime.genero);
+    console.log(anime.duracion);
+    console.log(anime.poster);
+    console.log(anime.sinopsis);
+    console.log(anime.clasificacion);
+    console.log(anime.idioma);
+})
